79609571593c78efcdb3754a68e4191091d27dee
Tamires <85920986+Tamires-Mendes@users.noreply.github.com>
2022-01-18 16:27:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2500
+  });
+
+
+  $('.slider-lancamento').slick({
+    infinite: true,
+    dots: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2500
+
+  });
+
+  $('.slider-pre-lancamento').slick({
+    infinite: true,
+    dots: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2500
+
+  });
